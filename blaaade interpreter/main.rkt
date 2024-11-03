b6d29a14b1eb43d1a59320538b0278bcd81de727
blade <yiming.liao@cuw.edu>
2024-11-03 00:06:13 -0500

    try the new way pass out return and environment

--- blaaade interpreter/main.rkt
+++ blaaade interpreter/main.rkt
@@ -7,11 +7,11 @@
   '(((a 1) (b 2) (x 5)))
   )
 
-;(define execute
-;  (lambda (code)
-;    (blaaade-interpreter (blaaade-parser code) var-env)
-;    )
-;  )
+(define execute
+  (lambda (code)
+    (blaaade-2-interpreter (blaaade-2-parser code) var-env)
+    )
+  )
 
 ;compiler
 ;((out-exp (num-exp 1)) ...)
@@ -19,7 +19,7 @@
 ;(out-exp return-value return-env)
 ;rest expressions return-value return-env
 ;out-exp what brings into the out-exp: value and env
-(blaaade-2-parser '((out 1)))
+(execute '((out 1) (out (2 + 3)) (out b)))
 
 ;(blaaade-2-interpreter (blaaade-2-parser '(out 1)) var-env)
 ;1
--- blaaade interpreter/parser-v2.rkt
+++ blaaade interpreter/parser-v2.rkt
@@ -7,7 +7,12 @@
       ((null? code) '())
       ;(1)
       ((number? code) (list 'num-exp code))
+      ((symbol? code) (list 'var-exp code))
       ((and (pair? code) (eq? (car code) 'out)) (list 'out-exp (blaaade-2-parser (cadr code))))
+      ((and (pair? code) (eq? (car code) 'put) (symbol? (cadr code)) (eq? (caddr code) '=))
+       (list 'put-exp (cadr code) (blaaade-2-parser (cadddr code))))
+      ((and (pair? code) (eq? (length code) 3) (math-op? (cadr code)))
+       (list 'math-exp (cadr code) (blaaade-2-parser (car code)) (blaaade-2-parser (caddr code))))
       ((pair? code)
        (cons (blaaade-2-parser (car code)) (blaaade-2-parser (cdr code))))
       (else (displayln "Unknown syntax error occurs."))
@@ -15,4 +20,17 @@
     )
   )
 
+(define math-op?
+  (lambda (op)
+    (cond
+      ((eq? op '+) #t)
+      ((eq? op '-) #t)
+      ((eq? op '/) #t)
+      ((eq? op '*) #t)
+      ((eq? op '%) #t)
+      (else #f)
+      )
+    )
+  )
+      
 (provide (all-defined-out))
